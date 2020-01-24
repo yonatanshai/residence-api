379681e9a91d283681211cebe117b41721ce23c1
shaiyonatan <shaiyonatan@gmail.com>
2020-01-24 10:41:32 +0200

    update config

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,19 +1,6 @@
 const mongoose = require('mongoose');
 
 const connect = async(cb = null) => {
-	// try {
-	//     await mongoose.connect(
-	//         `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-ngxlm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-	//         {
-	//             useNewUrlParser: true,
-	//             useUnifiedTopology: true,
-	//         }
-	//     )
-
-	//     cb();
-	// } catch (error) {
-	//     console.log(error);
-	// }
 	try {
 		await mongoose.connect(
 			process.env.DB_CONNECTION_STRING,
@@ -28,20 +15,7 @@ const connect = async(cb = null) => {
 	} catch (error) {
 		console.log(error);
 	}
-	// mongoose
-	// .connect(
-	//     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-ngxlm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-	//     {
-	//         useNewUrlParser: true,
-	//         useUnifiedTopology: true,
-	//     }
-	// )
-	// // .then(() => console.log((`mongoose connected to ${process.env.DB_NAME}`)))
-	// .then(cb)
-	// .catch(err => console.log(err));
 };
-
-// connect();
 
 module.exports = connect;
 
