e36373b072380828132977fc7923ebef207779c7
shaiyonatan <shaiyonatan@gmail.com>
2020-01-24 16:03:39 +0200

    changed project structure

--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const connectMongoose = require('./db/mongoose');
-const port = process.env.PORT || 5000;
-
-
-connectMongoose(() => app.listen(port, () => console.log(`Server running on port ${port}`)));
-// app.listen(port, () => console.log(`Server running on port ${port}`));
